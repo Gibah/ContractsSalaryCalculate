1b7b3b1ea086c8e25a3c79d61c1bd8b26d56649e
Gilberto <gilberto_silva.r@hotmail.com>
2022-05-17 19:08:57 -0300

    My first Contract's Salary Calculator

---
+++ ProjectComposition/Entities/HourContract.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectComposition.Entities
+{
+    class HourContract
+    {
+        public DateTime Date { get; set; }
+        public double ValuePerHour { get; set; }
+        public int Hours { get; set; }
+
+        public HourContract()
+        {
+        }
+
+        public HourContract(DateTime date, double valuePerHour, int hour)
+        {
+            Date = date;
+            ValuePerHour = valuePerHour;
+            Hours = hour;
+        }
+
+        public double TotalValue() 
+        {
+            return Hours * ValuePerHour;
+        }
+    }
+}
